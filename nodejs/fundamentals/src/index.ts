29be40c30b7ddca7100cdf35f96800e54d2298d3
Abner <abnerborgonha@gmail.com>
2022-01-08 00:43:44 -0300

    feat(fundamentals): create simple crud

--- nodejs/fundamentals/src/index.ts
+++ nodejs/fundamentals/src/index.ts
@@ -1,9 +1,61 @@
 import express from 'express'
 
+interface ICourse {
+  id: number
+  name: string
+  description: string
+  tags: string[]
+}
+
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Ignite' })
+app.use(express.json())
+
+let courses: ICourse[] = [
+  {
+    id: 1,
+    name: 'NodeJS',
+    description: 'NodeJS course by Abner Borgonha',
+    tags: ['backend', 'node', 'express']
+  }
+]
+
+app.get('/courses', (request, response) => {
+  return response.json(courses)
+})
+
+app.post('/courses', (request, response) => {
+  const data = request.body
+
+  courses.push(data)
+
+  return response.json(courses)
+})
+
+app.put('/courses/:id', (request, response) => {
+  const { id: courseId } = request.params
+  const data: ICourse = request.body
+
+  const updatedCourse = courses.filter(course => {
+    if (!(course.id === Number(courseId))) return course
+  })
+
+  courses = updatedCourse
+  courses.push(data)
+  
+  return response.json(courses)
+})
+
+app.delete('/courses/:id', (request, response) => {
+  const { id: courseId } = request.params
+
+  const deletedCourse = courses.filter(course => {
+    if (!(course.id === Number(courseId))) return course
+  })
+
+  courses = deletedCourse
+  
+  return response.status(204).send()
 })
 
 app.listen(3033, () => {
